a23a5ae165a3e016a1425207f311a7db9f26d91a
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-05 16:55:20 +0700

    Initializing a package

--- main.py
+++ main.py
@@ -9,26 +9,36 @@ if __name__ == "__main__":
   from module import function
   """
 
-  import sales.billing
-  import sales.delivery
-  import sales.order
+  # import sales.billing
+  # import sales.delivery
+  # import sales.order
+
+  # sales.order.create_sales_order()
+  # sales.delivery.create_delivery()
+  # sales.billing.create_billing()
+
+  # from sales.order import create_sales_order
+  # from sales.delivery import create_delivery
+  # from sales.billing import create_billing
+
+  # create_sales_order()
+  # create_delivery()
+  # create_billing()
+
+  # from sales.order import create_sales_order as create_order
+  # from sales.delivery import create_delivery as start_delivery
+  # from sales.billing import create_billing as issue_billing
+
+  # create_order()
+  # start_delivery()
+  # issue_billing()
+
+  # Initializing a package
+
+  from sales import TAX_RATE
+
+  print(TAX_RATE)
+
+  import sales
 
   sales.order.create_sales_order()
-  sales.delivery.create_delivery()
-  sales.billing.create_billing()
-
-  from sales.order import create_sales_order
-  from sales.delivery import create_delivery
-  from sales.billing import create_billing
-
-  create_sales_order()
-  create_delivery()
-  create_billing()
-
-  from sales.order import create_sales_order as create_order
-  from sales.delivery import create_delivery as start_delivery
-  from sales.billing import create_billing as issue_billing
-
-  create_order()
-  start_delivery()
-  issue_billing()
---
+++ sales/__init__.py
@@ -0,0 +1,5 @@
+# import the order module automatically
+from sales.order import create_sales_order
+
+# default sales tax rate
+TAX_RATE = 0.07
